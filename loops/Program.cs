8a3658ab41b1d2a9bcad183edbd874084419d593
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-08-29 18:27:37 +0530

    Add project files.

---
+++ loops/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your target?");
+            int Target = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("______________________________________________");
+
+            int Start = 0;
+
+            while (Start <= Target)
+            {
+                Console.WriteLine(Start);
+                Start += 2;
+            }
+            Console.ReadLine();
+        }
+    }
+}
